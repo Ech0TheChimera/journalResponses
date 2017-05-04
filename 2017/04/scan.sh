7e1cb86f613f393bab6c5fa011465b3d42972b64
Ech0TheChimera <AaronWhittington8284@gmail.com>
2017-05-04 11:16:47 -0700

    Commit scan update 2

--- 2017/04/scan.sh
+++ 2017/04/scan.sh
@@ -2,6 +2,9 @@
 #FILES=/home/echo/Documents/ccnth/11/politicalStudies/journalResponses/2017/04/*
 for f in /home/echo/Documents/ccnth/11/politicalStudies/journalResponses/2017/04/*.txt 
 do
+	echo
+	echo
 	echo Processing $f
+	echo
 	grep "response = " $f
 done
